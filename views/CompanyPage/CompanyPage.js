8b6c91d762ebc6c39675e7dd040e2eb40f29e964
Nazar Yavnyy <nazar.yavnyy@gmail.com>
2018-05-24 10:39:03 +0300

    company page styles

--- views/CompanyPage/CompanyPage.js
+++ views/CompanyPage/CompanyPage.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
 
 import styles from './styles';
+
+const labels = {
+  disloyal: 'Порушник',
+  probation: 'Випробувальний',
+  loyal: 'Лояльна',
+};
 
 export default class CompanyPage extends React.Component {
   render() {
     const { company } = this.props;
     return (
       <View style={styles.container}>
-        {company && <Text>{company._id}</Text>}
+        <View style={styles.logoWrapper}>
+          <Image
+            style={{ width: 200, height: 200 }}
+            source={{ uri: company.img }}
+          />
+        </View>
+        <View style={[styles.loyaltyWrapper, styles[company.loyalty]]}>
+          <Text style={styles.loyalty}>{labels[company.loyalty]}</Text>
+        </View>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.title}>{company.title}</Text>
+          <Text style={styles.description}>{company.description}</Text>
+        </View>
       </View>
     );
   }
--- views/CompanyPage/styles.js
+++ views/CompanyPage/styles.js
@@ -4,8 +4,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  logoWrapper: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  loyaltyWrapper: {
+    height: 40,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loyalty: {
+    fontSize: 18,
+    color: 'white',
+  },
+  loyal: {
+    backgroundColor: 'rgba(110,157,58, 0.8)',
+  },
+  disloyal: {
+    backgroundColor: 'rgba(165,41,41, 0.8)',
+  },
+  probation: {
+    backgroundColor: 'rgba(247,149,33, 0.8)',
+  },
+  infoWrapper: {
+    marginTop: 40,
+    padding: 20,
+    flex: 1,
+    backgroundColor: 'rgba(0, 74, 128, 0.10)',
+  },
+  title: {
+    fontSize: 20,
+  },
+  description: {
+    fontSize: 14,
+    marginTop: 8,
+    color: '#666',
   },
 });
 
